5555ac52f0b5456d2c33a3105ebbefeae5951782
crismazzei <71802221+crismazzei@users.noreply.github.com>
2024-12-20 02:34:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+import Home from './Home';
+import Login from './Login';
+import Cadastro from './arearestrita/Cadastro';
+import Perfil from './arearestrita/Perfil';
+import Reservas  from './arearestrita/Reservas'
+import 'react-native-gesture-handler';
+
+import BotaoConfirme from '../assets/components/BotaoConfirme';
+
+import Confirmacao from './arearestrita/Confirmacao';
+import Relogio from '../assets/components/Relogio'
+import Realizados from './arearestrita/Realizados';
+import PerfilAdm from './arearestrita/PerfilAdm'
+import ListaUsuarios from './arearestrita/listas/ListaUsuarios';
+import ListaUsuariosBusca from './arearestrita/listas/ListaUsuariosBusca';
+
+export { 
+    Home, 
+    Login, 
+    Cadastro, 
+    Perfil, 
+    BotaoConfirme, 
+    Confirmacao, 
+    Relogio, 
+    Reservas, 
+    Realizados,
+    PerfilAdm,
+    ListaUsuarios,
+    ListaUsuariosBusca
+};
